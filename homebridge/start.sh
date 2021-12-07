aa480b6c7de89f1eb2cf352a40ab9bf30cc73b77
Erik de Wildt <erik.de.wildt@gmail.com>
2021-12-07 15:02:02 +0100

    Version 0.0.20

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
+
+# Create data dir, place default config if not exist.
 mkdir -p /data
 if [[ ! -f /data/config.json ]]
 then
   cp /root/.homebridge/config.json /data/config.json
 fi
 
-if [[ ! -d /data/lib ]]
-then
-  mv /usr/local/lib /data
-fi
+# Remove old versions if exist.
+rm -rf /data/lib/node_modules/homebridge
+rm -rf /data/lib/node_modules/homebridge-config-ui-x
+rm -rf /data/lib/node_modules/ffmpeg-for-homebridge
+
+# Copy node_module packages to /data
+cp -pr /usr/local/lib /data
+
+# Remove node modules from /usr/local/lib to create symlink to /data/lib
 rm -rf /usr/local/lib
+
+# Create symlink from /data to /usr/local for persistence.
 ln -s /data/lib /usr/local/lib
 
-/data/lib/node_modules/homebridge/bin/homebridge -U /data | tee /var/log/homebridge.log &
-nginx -c /etc/nginx/nginx.conf
+/data/lib/node_modules/homebridge/bin/homebridge -U /data | tee /var/log/homebridge.log
